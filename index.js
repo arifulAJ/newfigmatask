49b850d9c883fb2353d89274496175edc4ae404d
arifulislam10111999 <arifulislam10111999@gmail.com>
2022-01-20 21:35:57 +0600

    all conde

---
+++ index.js
@@ -0,0 +1,7 @@
+const bar=document.querySelector(".menubar");
+const cont=document.querySelector(".navkey");
+bar.addEventListener("click",()=>{
+    cont.classList.toggle("hidden")
+    console.log("ksdlflsdjfjsljk");
+    
+})
