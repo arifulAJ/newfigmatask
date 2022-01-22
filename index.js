07da07fa4370c4a8f5337560d20e48518851bc27
arifulislam10111999 <arifulislam10111999@gmail.com>
2022-01-22 16:07:14 +0600

    nav bar fix

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-const bar=document.querySelector(".menubar");
-const cont=document.querySelector(".navkey");
-bar.addEventListener("click",()=>{
-    cont.classList.toggle("hidden")
-    console.log("ksdlflsdjfjsljk");
-    
-})
+function showMenu() {
+    var bar=document.getElementById("bar");
+    var time=document.getElementById("time");
+    var menu =document.getElementById("mobileM");
+    bar.style.display="none";
+    time.style.display="block";
+    menu.style.display="block";
+
+}
+function closeMenu() {
+    var bar=document.getElementById("bar");
+    var time=document.getElementById("time");
+    var menu =document.getElementById("mobileM");
+    bar.style.display="block";
+    time.style.display="none";
+    menu.style.display="none";
+
+}
